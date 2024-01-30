0f663052f08156f9c7c939418dade6b872b11486
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-01-30 09:56:10 +0100

    Creado movimiento objetos

--- Assets/Scripts/MoverObjeto.cs
+++ Assets/Scripts/MoverObjeto.cs
@@ -1,31 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class BasicRigidBodyPush : MonoBehaviour
+public class MovimientoObjeto : MonoBehaviour
 {
-	public LayerMask pushLayers;
+
+	private float masaObjeto;
 	public bool canPush;
 	[Range(0.5f, 5f)] public float strength = 1.1f;
-	
+
 
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
 		if (canPush) PushRigidBodies(hit);
 	}
 
-	private void PushRigidBodies(ControllerColliderHit hit)
-	{
-		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
-
-		// make sure we hit a non kinematic rigidbody
+	private void PushRigidBodies(ControllerColliderHit hit)	{
+		
 		Rigidbody body = hit.collider.attachedRigidbody;
-		if (body == null || body.isKinematic) return;
+		if (body == null || body.isKinematic)
+		{
+			return;
+		}
 
-		// make sure we only push desired layer(s)
-		var bodyLayerMask = 1 << body.gameObject.layer;
-		if ((bodyLayerMask & pushLayers.value) == 0) return;
 
 		// We dont want to push objects below us
-		if (hit.moveDirection.y < -0.3f) return;
+		if (hit.moveDirection.y < -0.3f)
+		{
+			return;
+		}
+
+		masaObjeto=body.mass;
 
 		// Calculate push direction from move direction, horizontal motion only
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
