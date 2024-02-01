bf83c7fcad004a21864e6eb14c5560c94cfd3c97
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-02-01 09:27:26 +0100

    Mejorado crear marca

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+
+
 public class Teleport : MonoBehaviour
 {
 
     public GameObject Marca;
+    public bool crearMarca=false;
     
 
   
     void Update()
     {
-        if (Keyboard.current.mKey.isPressed)
+        if (Keyboard.current.mKey.isPressed && !crearMarca)
         {
             CrearMarca();
+            crearMarca = true;
 
 
         }
@@ -26,14 +31,12 @@ public class Teleport : MonoBehaviour
         // Obtén la posición del jugador
         Vector3 posicionJugador = transform.position;
 
-        // Ajusta la posición del objeto "Marca" para que sea más visible (ajusta según tus necesidades)
-        posicionJugador += new Vector3(0, 1, 0); // Ajusta la altura si es necesario
-
         // Crea la "Marca" en la posición del jugador
         GameObject nuevaMarca = Instantiate(Marca, posicionJugador, Quaternion.identity);
 
-        // Asegúrate de que la instancia tenga la misma escala que el prefab
-        nuevaMarca.transform.localScale = Marca.transform.localScale;
+     
+
+       
 
     }
 }
